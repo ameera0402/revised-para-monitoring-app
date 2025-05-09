3dfb85bcea346449fea47a95d81fdbe05d29f021
ameera0402 <cabusao.ameera@gmail.com>
2025-05-10 07:41:46 +0800

    Add files via upload

---
+++ StressMonitor.js
@@ -0,0 +1,6 @@
+
+import React, { useState, useEffect } from 'react';
+// Other React code will be placed here (optimized version of your existing code)
+export default function StressMonitor() {
+    return <div>Commuter Stress Monitoring App (Optimized)</div>;
+}
